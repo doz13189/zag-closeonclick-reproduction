fc7d256c612f832e4d99bdbd105e579380cc078b
Takahiro Kato <dozo13189@gmail.com>
2025-08-26 16:33:40 +0900

    reproduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { normalizeProps, useMachine } from "@zag-js/react"
+import * as tooltip from "@zag-js/tooltip"
+
+export function Tooltip({
+  closeOnClick,
+  closeOnPointerDown
+}: { closeOnClick: boolean, closeOnPointerDown: boolean }) {
+  const service = useMachine(tooltip.machine, { id: "1", closeOnClick, closeOnPointerDown })
+
+  const api = tooltip.connect(service, normalizeProps)
+
+  return (
+    <div>
+      <button {...api.getTriggerProps()}>Hover me</button>
+      {api.open && (
+        <div {...api.getPositionerProps()}>
+          <div {...api.getContentProps()}>Tooltip</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      {/* work */}
+      <Tooltip closeOnClick={false} closeOnPointerDown={false} />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      {/* not work */}
+      <Tooltip closeOnClick={true} closeOnPointerDown={false} />
+
+      {/* not work */}
+      <Tooltip closeOnClick={false} closeOnPointerDown={true} />
+    </main>
   );
 }
